d72a48f6621bbfbd6b73b907385e6f4bf247711a
Yassouridis <christina.yassouridis@stat-up.com>
2016-08-05 17:49:28 +0200

    plots changed

--- R/plots.R
+++ R/plots.R
@@ -10,7 +10,7 @@
 #' @description Auxiliary function function for plotting graphical convergence
 #'  diagnostics for Bayes models
 #' @usage diagnostics(nodes, plots=FALSE)
-#' @param S TO DESCRIBE
+#' @param x runjags object
 #' @param nodes character string, the name of parameters(s)
 #' @param plotnumber single numerical value (1 for plotting Gelman-Rubin convergence
 #'  statistic (\code{samplesBgr()}), 2 for plotting history (\code{plotDensity}),
@@ -18,20 +18,40 @@
 #' @keywords manip
 #' @export
 
-plotDiag <- function(S, plotnumber=1){
-  switch(plotnumber,
+plotDiag <- function(x, plot.type="all"){
+  switch(plot.type,
          #Gelman-Rubin - Old: rrisk.sampleBgr
-         "1" = ggs_Rhat(S) ,
+         "gelman" = gelman.plot(x) ,
 
          #Traceplot - Old: plotHistory
-         "2" = ggs_traceplot(S),
-
+         "trace" = plot(x, plot.type= "trace"),
 
          #Autocorrelation - Old: plotAutoC
-         "3" = ggs_autocorrelation(S),
+         "autocorr" = plot(x, plot.type = "autocorr"),
+
+         #Histogram
+         "histogram" = plot(x, plot.type = "histogram"),
 
          #Density - Old: plotDensity
-         "4" = ggs_density(S)
+         "density" = {
+           mcmcobj <- as.mcmc(x)
+           par(mfrow=c(1,3))
+           densplot(mcmcobj)
+           par(mfrow=c(1,1))
+         },
+
+         "all" =
+         {
+           gelman.plot(x)
+           plot(x, plot.type= "trace")
+           plot(x, plot.type = "autocorr")
+           plot(x, plot.type = "histogram")
+           mcmcobj <- as.mcmc(x)
+           par(mfrow=c(1,3))
+           densplot(mcmcobj)
+           par(mfrow=c(1,1))
+         }
+
   )
 }
 
